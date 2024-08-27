16dc4b3d60db1678ebf7f74eae64840185520fe9
ViniciusCampeao <viniciusfernandes643@gmail.com>
2024-08-27 14:52:34 -0300

    fix: fixed some details

---
+++ src/components/Carousel/partials/export.tsx
@@ -0,0 +1,54 @@
+import CarouselComponent from "..";
+
+const data = [
+  {
+    imageUrl: "url-da-imagem-1",
+    city: "Paris",
+    country: "France",
+    rating: 4,
+    reviews: 120,
+    duration: 7,
+    price: 1299,
+  },
+  {
+    imageUrl: "url-da-imagem-1",
+    city: "Paris",
+    country: "France",
+    rating: 4,
+    reviews: 120,
+    duration: 7,
+    price: 1299,
+  },
+  {
+    imageUrl: "url-da-imagem-1",
+    city: "Paris",
+    country: "France",
+    rating: 4,
+    reviews: 120,
+    duration: 7,
+    price: 1299,
+  },
+  {
+    imageUrl: "url-da-imagem-1",
+    city: "Paris",
+    country: "France",
+    rating: 4,
+    reviews: 120,
+    duration: 7,
+    price: 1299,
+  },
+  {
+    imageUrl: "url-da-imagem-1",
+    city: "Paris",
+    country: "France",
+    rating: 4,
+    reviews: 120,
+    duration: 7,
+    price: 1299,
+  },
+];
+
+const ExportCarousel1 = () => {
+  return <CarouselComponent data={data} />;
+}
+export default ExportCarousel1;
