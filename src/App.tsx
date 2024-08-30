b7a54e27b1bdfc3ad9474abf77c0070cefe4d64c
ViniciusCampeao <viniciusfernandes643@gmail.com>
2024-08-30 00:59:55 -0300

    feat: integration of private routes where necessary

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute.tsx';
+import { AuthProvider } from './context/AuthContext';
+import Tours from './pages/Tours';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/tours"
+            element={
+              <PrivateRoute element={<Tours />} />
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
